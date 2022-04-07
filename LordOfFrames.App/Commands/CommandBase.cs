0e2c8f5f4a866301a7883e452d6061bb65eb5ecd
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-06 20:57:13 -0400

    CommandBase and standard commands

---
+++ LordOfFrames.App/Commands/CommandBase.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace LordOfFrames.App.Commands;
+
+public class CommandBase : ModuleBase<SocketCommandContext>
+{
+    protected SocketUser User => Context.Message.Author;
+    protected IGuildUser GuildUser => (IGuildUser)User;
+    protected string Mention => User.Mention;
+}
---
+++ LordOfFrames.App/Commands/HelpCommand.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Commands;
+
+namespace LordOfFrames.App.Commands;
+
+public class HelpCommand : CommandBase
+{
+    [Command("help")]
+    public async Task HandleCommandAsync()
+    {
+        await ReplyAsync($"**The following commands can be used:**{Environment.NewLine}" +
+                         $"  • **ping** - Pings the Discord channel{Environment.NewLine}");
+    }
+}
