b71103a702afed4e8476fc899e38fd34ec58c980
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-09 06:58:38 -0400

    Added ListGames command

---
+++ LordOfFrames.App/Commands/ListGamesCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Discord.Commands;
+using LordOfFrames.Database;
+
+namespace LordOfFrames.App.Commands;
+
+public class ListGamesCommand : CommandBase
+{
+    private GameRepository _repository;
+
+    public ListGamesCommand(GameRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    [Command("listgames")]
+    public async Task HandleCommandAsync()
+    {
+        var sb = new StringBuilder();
+        var games = await _repository.GetAll();
+        foreach (var game in games.OrderBy(g => g.Name))
+        {
+            sb.Append($"- {game.Name} ({game.Id})\n");
+        }
+        var dmChannel = await GuildUser.CreateDMChannelAsync();
+        await dmChannel.SendMessageAsync($"Game List:\n{sb}");
+    }
+}
