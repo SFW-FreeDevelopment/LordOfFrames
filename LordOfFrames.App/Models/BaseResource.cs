6a5f789a6d7d4319fb47f0df89324f48cdaaba37
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-09 03:50:32 -0400

    Updated BaseResource

--- LordOfFrames.App/Models/BaseResource.cs
+++ LordOfFrames.App/Models/BaseResource.cs
@@ -5,4 +5,7 @@ public abstract class BaseResource
     public string Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int Version { get; set; }
 }
