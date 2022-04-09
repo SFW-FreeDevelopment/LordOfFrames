e06b5992b86b9ebdfbac944468ae2fa151aa11f3
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-09 03:50:45 -0400

    Added GameRepository

--- LordOfFrames.App/Bot.cs
+++ LordOfFrames.App/Bot.cs
@@ -2,6 +2,7 @@
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using LordOfFrames.App.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LordOfFrames.App;
@@ -20,6 +21,7 @@ public static class Bot
         _services = new ServiceCollection()
             .AddSingleton(_client)
             .AddSingleton(_commands)
+            .AddScoped<GameRepository>()
             .BuildServiceProvider();
 
         const string token = $"{Constants.BotToken}";
