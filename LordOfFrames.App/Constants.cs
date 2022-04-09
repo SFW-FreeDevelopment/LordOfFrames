ed4ecb07247d40ef9006c9cb7a2aaee4fe9aff7e
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-09 05:28:15 -0400

    Added Mongo connection string

--- LordOfFrames.App/Constants.cs
+++ LordOfFrames.App/Constants.cs
@@ -10,4 +10,5 @@ public static class Constants
     public const string PingMessage = "I am pinging the server.";
     public const string MongoDatabaseUsername = "admin";
     public const string MongoDatabasePassword = "CHANGE_ME";
+    public static readonly string MongoDatabaseConnectionString = $"mongodb+srv://{MongoDatabaseUsername}:{MongoDatabasePassword}@cluster0.ws062.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 }
