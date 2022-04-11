55561a6e624a1b165e2f934f367e273aef31b7d5
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-11 13:30:18 -0400

    MoveType now required

--- LordOfFrames.Models/Move.cs
+++ LordOfFrames.Models/Move.cs
@@ -1,17 +1,18 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace LordOfFrames.Models;
 
 public class Move : Base<Move>
 {
-    public string Input { get; set; }
+    [Required] public string Input { get; set; }
     public int? Startup { get; set; }
     public int? Active { get; set; }
     public int? OnHit { get; set; }
     public int? OnBlock { get; set; }
     public int? Recovery { get; set; }
-    public MoveType? Type { get; set; }
+    [Required] public MoveType Type { get; set; }
 }
 
 [JsonConverter(typeof(StringEnumConverter))]
