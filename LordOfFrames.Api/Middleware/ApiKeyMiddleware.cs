14ae37abdadecd5469bffd3a531af1a7f5a81f95
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-16 21:30:08 -0400

    Add ApiKeyMiddleware

---
+++ LordOfFrames.Api/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace LordOfFrames.Api.Middleware;
+
+public class ApiKeyMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private const string ApiKey = "XApiKey";
+
+    public ApiKeyMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(ApiKey, out
+                var extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            return;
+        }
+
+        var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+        var apiKey = appSettings.GetValue<string>(ApiKey);
+        if (!apiKey.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            return;
+        }
+
+        await _next(context);
+    }
+}
