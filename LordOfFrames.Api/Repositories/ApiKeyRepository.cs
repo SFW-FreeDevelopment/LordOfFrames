67af3a26d6f569b07fc8ac7c2fd70a53a16cef8f
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-30 13:36:15 -0400

    ApiKeyRepository

---
+++ LordOfFrames.Api/Repositories/ApiKeyRepository.cs
@@ -0,0 +1,29 @@
+﻿using LordOfFrames.Api.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace LordOfFrames.Api.Repositories;
+
+public class ApiKeyRepository
+{
+    private readonly IMongoClient _mongoClient;
+
+    public ApiKeyRepository(IMongoClient mongoClient)
+    {
+        _mongoClient = mongoClient;
+    }
+    
+    public async Task<bool> ValidateKey(string key) 
+    {
+        var apiKey = await GetCollection().AsQueryable()
+            .FirstOrDefaultAsync(a => a.Key.Equals(key));
+        return apiKey != null;
+    }
+    
+    private IMongoCollection<ApiKey> GetCollection()
+    {
+        IMongoDatabase database = _mongoClient.GetDatabase("lordofframes");
+        IMongoCollection<ApiKey> collection = database.GetCollection<ApiKey>("apikeys");
+        return collection;
+    }
+}
