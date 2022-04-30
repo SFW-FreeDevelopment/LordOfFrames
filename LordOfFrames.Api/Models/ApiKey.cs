2bf33533181e62ff42fa2df035c7c4cdd8498b43
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-30 13:36:02 -0400

    New ApiKey model

---
+++ LordOfFrames.Api/Models/ApiKey.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace LordOfFrames.Api.Models;
+
+internal class ApiKey
+{
+    [BsonId]
+    public string Id { get; set; }
+    public string Key { get; set; }
+    public string Assignee { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
